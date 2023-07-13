878bfeae24cb415cee7ebf2206e9ca38f0ba5634
Vishal <vishalgawde07011991@gmail.com>
2023-07-13 13:58:56 +0530

    Initial Pilot Files

---
+++ basic-springboot-web/src/main/java/com/learning/basicspringbootweb/BasicSpringbootWebApplication.java
@@ -0,0 +1,13 @@
+package com.learning.basicspringbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringbootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringbootWebApplication.class, args);
+	}
+
+}
---
+++ basic-springboot-web/src/test/java/com/learning/basicspringbootweb/BasicSpringbootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.basicspringbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringbootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
